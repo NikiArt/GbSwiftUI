5782bfe051c28a74f0bda43359ed23b51b675d71
Nikita Boiko <genertyniki@gmail.com>
2019-11-24 21:28:20 +0300

    Initial Commit

---
+++ GbSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GbSwiftUI
+//
+//  Created by Nikita Boiko on 24.11.2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
