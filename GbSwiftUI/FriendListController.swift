49627d605ff5361f08c00332a95606467320ee86
Nikita Boiko <genertyniki@gmail.com>
2019-12-17 22:05:24 +0300

    realise sections in friend list

--- GbSwiftUI/FriendListController.swift
+++ GbSwiftUI/FriendListController.swift
@@ -17,12 +17,16 @@ class FriendListController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return DataBinder.instance.userList.count
+        return DataBinder.instance.friendsSection[section].items.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return DataBinder.instance.friendsSection.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as! FriendCell
-        cell.friendName.text = DataBinder.instance.userList[indexPath.row].name
+        cell.friendName.text = DataBinder.instance.friendsSection[indexPath.section].items[indexPath.row].name
         cell.photo.image.image = UIImage(named: "ava")
         return cell
     }
@@ -30,9 +34,14 @@ class FriendListController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(identifier: "Photos") as! Photos
-        viewController.user = DataBinder.instance.userList[indexPath.row]
+        
+        viewController.user = DataBinder.instance.friendsSection[indexPath.section].items[indexPath.row]
         self.navigationController?.pushViewController(viewController, animated: true)
         
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return DataBinder.instance.friendsSection[section].text
     }
 
 }
--- GbSwiftUI/model/DataBinder.swift
+++ GbSwiftUI/model/DataBinder.swift
@@ -8,11 +8,18 @@
 
 import Foundation
 
+struct Section<T> {
+    var text: String
+    var items: [T]
+    
+}
+
 final class DataBinder {
     
     static let instance = DataBinder()
     let userList: [User]
     var groupList: [Group]
+    var friendsSection = [Section<User>]()
     
     init() {
         userList = [
@@ -34,6 +41,18 @@ final class DataBinder {
             Group(name: "Рисуем все"),
             Group(name: "Geekbrains")
         ]
+        
+        getSectionStructure()
+    }
+    
+    func getSectionStructure() {
+        let sortedFriendList = userList.sorted{$0.name < $1.name}
+        
+        let friendDictionary = Dictionary.init(grouping: sortedFriendList) {
+            $0.name.prefix(1)
+        }
+        friendsSection = friendDictionary.map{Section(text: String($0.key), items: $0.value)}
+        friendsSection.sort{$0.text < $1.text}
     }
     
     
