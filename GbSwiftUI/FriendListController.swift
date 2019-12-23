b12a20312d2ae0ded5591b97343bd9d76406e0c3
Nikita Boiko <genertyniki@gmail.com>
2019-12-23 23:06:00 +0300

    add search bar

--- GbSwiftUI/FriendListController.swift
+++ GbSwiftUI/FriendListController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class FriendListController: UITableViewController {
 
+    @IBOutlet weak var friendSearch: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        friendSearch.delegate = self
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,5 +45,29 @@ class FriendListController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return DataBinder.instance.friendsSection[section].text
     }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return DataBinder.instance.friendsSection.map{$0.text}
+    }
 
+}
+
+extension FriendListController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let friendDictionary = Dictionary.init(grouping: DataBinder.instance.userList.filter { user -> Bool in
+            return searchText.isEmpty ? true : user.name.lowercased().contains(searchText.lowercased())
+            
+        }) { $0.name.prefix(1)}
+        
+        DataBinder.instance.friendsSection = friendDictionary.map{ Section(text: String($0.key), items: $0.value)}
+        DataBinder.instance.friendsSection.sort{$0.text < $1.text}
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    
 }
