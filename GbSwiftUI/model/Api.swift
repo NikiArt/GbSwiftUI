a8447165840e12b0ce7e005d392864b1b086acf7
Nikita Boiko <genertyniki@gmail.com>
2020-01-19 08:53:22 +0300

    почему-то не работало чете IBOutlet при создании webView. Реализовал программное создание, как у преподавателя Реализовал получение токена

---
+++ GbSwiftUI/model/Api.swift
@@ -0,0 +1,22 @@
+//
+//  Api.swift
+//  GbSwiftUI
+//
+//  Created by Nikita Boiko on 15.01.2020.
+//  Copyright © 2020 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Api {
+    static let shared = Api()
+    
+    let appId = "6768580"
+    let key = "YuQ9DOn25t0BhtZSWCbV"
+    let serviceKey = "5335221d5335221d5335221ddc535265d9553355335221d0f373febfb5b04c63e9df51a"
+    var sessionToken = ""
+    
+    private init() {}
+    
+}
---
+++ GbSwiftUI/ui/LoginController.swift
@@ -0,0 +1,70 @@
+//
+//  LoginController.swift
+//  GbSwiftUI
+//
+//  Created by Nikita Boiko on 15.01.2020.
+//  Copyright © 2020 Nikita Boiko. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class LoginController: UIViewController {
+    
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let webViewConfig = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webViewConfig)
+        webView.navigationDelegate = self
+        
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Api.shared.appId),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.68")
+        ]
+        
+        //let request = URLRequest(url: urlComponents.url!)
+        let request = URLRequest(url: urlComponents.url!)
+        webView.load(request)
+        view = webView
+    }
+
+}
+
+extension LoginController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        //decisionHandler(.allow)
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+
+        let token = params["access_token"]
+        Api.shared.sessionToken = token ?? ""
+        print(token)
+
+
+        decisionHandler(.cancel)
+    }
+}
